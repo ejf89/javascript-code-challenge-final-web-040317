b6cad575bf40a080f2855cdb5e877749d65daf76
github username <github email address>
2017-06-02 09:48:25 -0400

    basic funky

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // your code here!
+  $("#photo-form").submit(function(event){
+      event.preventDefault()
+
+      let link = $("#linkInput").val()
+      let caption = $("#capInput").val()
+      photoPoster(link, caption)
+      this.reset()
+  })
 })
+
+function photoPoster(link, caption){
+
+    let img = `<img src="${link}">`
+    let comment = `<p>${caption}</p>`
+
+    var list = $("#photo-list")
+
+    list.append(img)
+    list.append(comment)
+}
+
+
+// function clearForm(){
+//     debugger
+//     this.reset()
+// }
